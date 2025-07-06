3eb1ead7ff5a9afb07c549052053bbe3842a8585
Manpreet <manpreetbhasin@gmail.com>
2025-07-06 00:22:49 -0700

    Adding htaccess file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <h3> Contents</h3>
       <div className="dotted-link-container">
         <div className="dotted-link-item">
-          <span className="dotted-link-text">Basic Setup</span>
+          <span className="dotted-link-text">0.1 Basic Setup</span>
           <span className="dotted-line" />
           <a href="/001" className="dotted-link">
             Link
